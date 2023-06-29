8a1538184ab3f304fd99aaba79976680305344de
Cem Bıçakcı <cmbicakci@gmail.com>
2023-06-29 22:05:50 +0300

    arrays

---
+++ 1-Variables.playground/Contents.swift
@@ -0,0 +1,47 @@
+import UIKit
+
+//Variables & Constants
+
+//String
+
+// var ile tanımlanan değişkenler, daha sonra değiştirilebilir.
+var greeting = "Hello, playground"
+
+var number = 10
+number = 20
+
+var name = "Cem"
+name.append("a") //sonuna ekler
+name.lowercased() // hepsi küçük
+name.uppercased() //hepsi büyük
+
+print(number)
+
+// let ile tanımlanan değişkenler, daha sonra değiştirilemez
+let userAge = 30
+
+
+//Integer
+
+let age = 50
+let myAge = 4
+age / myAge
+//12
+
+//Double
+
+let ageD = 50.0
+let myAgeD = 4.0
+ageD / myAgeD
+//12.5
+
+//Booelan
+
+var myBooelan = false
+
+// *********
+
+var myString : String = "10"
+let myNumber : Int = 20
+
+let stringNumber : String = String(20)
---
+++ 2-Arrays.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+var myFavouriteArray = ["Truman Show", "The Green Mile", "Pulp Fiction"]
+
+var myArray = ["Truman Show", "The Green Mile", 5, true] as [Any]
+
+//as => casting
+//any => any object
+
+var myStrArray = ["Test1", "Test2", "Test3"]
+myStrArray[0].uppercased()
+
+myStrArray.count
+
+myStrArray.last
+myStrArray[myStrArray.count - 1]
+
+myStrArray.sort()
