218cf44ab302a1bf47150026514392c6c360ba2c
Cem Bıçakcı <cmbicakci@gmail.com>
2023-06-30 13:19:23 +0300

    loops

---
+++ 3-Loops.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+// While Loop
+var number = 0
+
+while number < 10 {
+    //print(number)
+    number += 1
+}
+
+
+// For Loop
+var fruitArray = ["Banana", "Apple", "Orange"]
+
+for index in fruitArray{
+    print(index)
+}
+
+var myNumbers = [10, 20, 30, 40, 50]
+
+for item in myNumbers{
+    print(item / 5)
+}
+
+
+for newInteger in 1 ... 5 {
+    print(newInteger)
+}
