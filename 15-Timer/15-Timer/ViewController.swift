d81dfd8f56fa0773681d193aaed0a7a1a8147673
Cem Bıçakcı <cmbicakci@gmail.com>
2023-07-01 15:46:47 +0300

    timer

--- 15-Timer/15-Timer/ViewController.swift
+++ 15-Timer/15-Timer/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var timerLabel: UILabel!
+    
+    var timer = Timer()
+    var counter = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        counter = 10
+        timerLabel.text = "Time: \(counter)"
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(TimerFunc), userInfo: nil, repeats: true)
+        
+    }
+    
+    @objc func TimerFunc(){
+        timerLabel.text = "Time: \(counter)"
+        counter -= 1
+        
+        if(counter == 0){
+            //timer ı durdurdur.
+            timer.invalidate()
+            timerLabel.text = "Time is over"
+        }
     }
 
-
+    @IBAction func button(_ sender: Any) {
+        print("Button Clicked")
+    }
+    
 }
 
