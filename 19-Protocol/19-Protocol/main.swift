4284cb3326f81a2e3acd2cde47e4225294c21b54
Cem Bıçakcı <cmbicakci@gmail.com>
2023-07-22 23:07:58 +0300

    protocol

---
+++ 19-Protocol/19-Protocol/main.swift
@@ -0,0 +1,53 @@
+//
+//  main.swift
+//  19-Protocol
+//
+//  Created by Cem Bıçakcı on 22.07.2023.
+//
+
+import Foundation
+
+protocol Running {
+    func myRun()
+}
+
+class Animal {
+    func test() {
+        print("test")
+    }
+}
+
+class Dog : Running {
+    func myRun() {
+        print("running")
+    }
+}
+
+let barley = Dog()
+barley.myRun()
+
+class Cat : Animal, Running {
+    func myRun() {
+        print("running")
+    }
+}
+
+let cat = Cat()
+cat.test()
+cat.myRun()
+
+class Turtle : Animal {
+    
+}
+
+let leonardo = Turtle()
+
+
+struct Bird : Running {
+    func myRun() {
+        print("running")
+    }
+}
+
+let tweety = Bird()
+tweety.myRun()
